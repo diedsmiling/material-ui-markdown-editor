a791c81580763ef432e5fbaeeab71c231f4e8b04
lazarev <diedsmiling@gmail.com>
2016-11-29 09:55:06 +0200

    Improved toolbar formatting

--- src/MarkdownEditor/ToolbarPanel/Button/Button.js
+++ src/MarkdownEditor/ToolbarPanel/Button/Button.js
@@ -4,14 +4,15 @@ import FlatButton from 'material-ui/FlatButton'
 const Button = props => (
   <FlatButton
     onClick={props.onClick}
-    style={{ minWidth: '36px' }}
+    style={Object.assign({ minWidth: '36px' }, props.style)}
     icon={props.icon}
   />
 )
 
 Button.propTypes = {
   icon: PropTypes.element,
-  onClick: PropTypes.function
+  onClick: PropTypes.function,
+  style: PropTypes.object //eslint-disable-line
 }
 
 export default Button
--- src/MarkdownEditor/ToolbarPanel/ToolbarPanel.js
+++ src/MarkdownEditor/ToolbarPanel/ToolbarPanel.js
@@ -2,6 +2,8 @@ import React, { PropTypes } from 'react'
 import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar'
 import Bold from 'material-ui/svg-icons/editor/format-bold'
 import Italic from 'material-ui/svg-icons/editor/format-italic'
+import BulletsList from 'material-ui/svg-icons/editor/format-list-bulleted'
+import NumbersList from 'material-ui/svg-icons/editor/format-list-numbered'
 import { lightBlack } from 'material-ui/styles/colors'
 import Button from './Button'
 import { formatBold, formatItalic } from '../helpers/formatting'
@@ -10,12 +12,23 @@ const ToolbarPanel = props => (
   <Toolbar>
     <ToolbarGroup firstChild>
       <Button
+        style={{ marginLeft: 24 }}
         onClick={function clickBold() { formatBold(props.cm) }}
         icon={<Bold color={lightBlack} />}
       />
       <Button
         onClick={function clickBold() { formatItalic(props.cm) }}
         icon={<Italic color={lightBlack} />}
+      />
+      <ToolbarSeparator />
+      <Button
+        style={{ marginLeft: 24 }}
+        onClick={function clickBold() { formatBold(props.cm) }}
+        icon={<BulletsList color={lightBlack} />}
+      />
+      <Button
+        onClick={function clickBold() { formatItalic(props.cm) }}
+        icon={<NumbersList color={lightBlack} />}
       />
       <ToolbarSeparator />
     </ToolbarGroup>
