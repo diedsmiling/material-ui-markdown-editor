960fcfa9301120654c407b5d94e3a7b39e02bec2
lazarev <diedsmiling@gmail.com>
2016-11-30 21:42:15 +0200

    Minor refactor

--- src/MarkdownEditor/helpers/formatting.js
+++ src/MarkdownEditor/helpers/formatting.js
@@ -13,17 +13,17 @@ const isEmpty = string => string.length === 0
 const getText = (signature, text) =>
   isEmpty(text) ? getPlaceholderBySignature(signature) : text
 
-const getEndPosition = (codeMirror, text, selection) => {
+const getEndPosition = (codeMirror, text, isNotPlaceholder) => {
   const end = codeMirror.getCursor('end')
-  return text === selection ? end : incrementPosition(text.length, end)
+  return isNotPlaceholder ? end : incrementPosition(text.length, end)
 }
 
 const format = signature => (cm) => {
   const { codeMirror } = cm
   const selection = codeMirror.getSelection()
-  const start = codeMirror.getCursor('start')
   const text = getText(signature, selection)
-  const end = getEndPosition(codeMirror, text, selection)
+  const start = codeMirror.getCursor('start')
+  const end = getEndPosition(codeMirror, text, text === selection)
 
   codeMirror.replaceSelection(signature + text + signature)
   codeMirror.setSelection(
