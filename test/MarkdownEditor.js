ee62321670ef3fd2919e5c43bff50027fec17742
lazarev <diedsmiling@gmail.com>
2016-11-30 21:27:47 +0200

    Renamed component

--- test/MarkdownEditor.js
+++ test/MarkdownEditor.js
@@ -2,9 +2,9 @@ import React from 'react'
 import test from 'ava'
 import { shallow } from 'enzyme'
 import Markdown from '../src/MarkdownEditor/MarkdownEditor'
-import Toolbar from '../src/MarkdownEditor/ToolbarPanel/ToolbarPanel'
+import ToolbarPanel from '../src/MarkdownEditor/ToolbarPanel/ToolbarPanel'
 
 test('Should render Toolbar', (t) => {
   const wrapper = shallow(<Markdown />)
-  t.true(wrapper.contains(<Toolbar />))
+  t.true(wrapper.contains(<ToolbarPanel />))
 })
