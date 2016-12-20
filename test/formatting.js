4da17a460e1bc5c5a46308597b21fe1d3e3c57b9
lazarev <diedsmiling@gmail.com>
2016-12-20 20:15:58 +0200

    Code improvement

--- test/formatting.js
+++ test/formatting.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import test from 'ava'
 import CM from 'codemirror'
-import Codemirror from 'react-codemirror'
 import { mount } from 'enzyme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import Markdown from '../src/MarkdownEditor/MarkdownEditor'
 import { getCurrentFormat } from '../src/MarkdownEditor/formatting'
 
 let wrapper
-
+let cm
 test.beforeEach(() => {
   wrapper = mount(<Markdown />, {
     context: {
@@ -18,28 +17,26 @@ test.beforeEach(() => {
       muiTheme: React.PropTypes.object.isRequired
     }
   })
+  cm = wrapper.state().cm
 })
 
 test('Should recognise current format and provide it in a form of array', (t) => {
-  const format = getCurrentFormat(wrapper.state().cm)
+  const format = getCurrentFormat(cm)
   t.true(Array.isArray(format))
 })
 
 test('Should recognise format', (t) => {
-  const { cm } = wrapper.state()
   cm.codeMirror.setValue('** Foo **')
   t.deepEqual(getCurrentFormat(cm), ['strong'])
 })
 
 test('Should return empty array if no format is recognised', (t) => {
-  const { cm } = wrapper.state()
   cm.codeMirror.setValue('** Foo ** bar')
   cm.codeMirror.setCursor({ line: 0, ch: 11 })
   t.deepEqual(getCurrentFormat(cm), [])
 })
 
 test('Should recognise "ul" format', (t) => {
-  const { cm } = wrapper.state()
   const list = `- foo \n- bar`
   cm.codeMirror.setValue(list)
   CM.signal(cm.codeMirror, 'change', cm.codeMirror, { origin: '+input' })
@@ -47,7 +44,6 @@ test('Should recognise "ul" format', (t) => {
 })
 
 test('Should recognise "ol" format', (t) => {
-  const { cm } = wrapper.state()
   const list = `1. foo \n2. bar`
   cm.codeMirror.setValue(list)
   CM.signal(cm.codeMirror, 'change', cm.codeMirror, { origin: '+input' })
