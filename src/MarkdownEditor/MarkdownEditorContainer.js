84003101abe8ebe20360a5e72d461cdd83f7e28d
lazarev <diedsmiling@gmail.com>
2017-01-04 19:54:53 +0200

    Fixed react-tap-event-plugin version

--- src/MarkdownEditor/MarkdownEditorContainer.js
+++ src/MarkdownEditor/MarkdownEditorContainer.js
@@ -13,6 +13,7 @@ const MarkdownEditorContainer = ({ wrapInTheme }) => (
     </MuiThemeProvider>
   : <MarkdownEditor />
 )
+
 MarkdownEditorContainer.propTypes = {
   wrapInTheme: PropTypes.bool
 }
