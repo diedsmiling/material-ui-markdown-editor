58b57c3610b933d72152cfbcf2c155ce3b380ff8
lazarev <diedsmiling@gmail.com>
2017-01-26 21:37:28 +0100

    Added state to <LinkDialog />

--- src/MarkdownEditor/LinkDialog/LinkDialog.js
+++ src/MarkdownEditor/LinkDialog/LinkDialog.js
@@ -3,40 +3,59 @@ import Dialog from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
 import TextField from 'material-ui/TextField'
 
-const LinkDialog = ({ isDialogOpen, toggleDialog }) => (
-  <Dialog
-    title="Insert hyperlink"
-    actions={[
-      <FlatButton
-        label="Cancel"
-        primary
-        onTouchTap={toggleDialog}
-      />,
-      <FlatButton
-        label="Ok"
-        primary
-        keyboardFocused
-        onTouchTap={toggleDialog}
-      />
-    ]}
-    modal={false}
-    open={isDialogOpen}
-    onRequestClose={toggleDialog}
-  >
-    <TextField
-      id="url"
-      floatingLabelText="Url"
-      style={{
-        width: '100%'
-      }}
-    />
-  </Dialog>
-)
+export default class LinkDialog extends React.Component {
+  static propTypes = {
+    isDialogOpen: PropTypes.bool.isRequired,
+    cm: PropTypes.object.isRequired, //eslint-disable-line
+  }
 
+  constructor() {
+    super()
+    this.state = {
+      url: ''
+    }
+    this.onChange = this.onChange.bind(this)
+  }
 
-LinkDialog.propTypes = {
-  isDialogOpen: PropTypes.bool.isRequired,
-  toggleDialog: PropTypes.func.isRequired
+  onChange(e) {
+    this.setState({
+      url: e.target.value
+    })
+  }
+
+  render() {
+    const { isDialogOpen } = this.props
+    const { toggleDialog } = this.context
+    return (
+      <Dialog
+        title="Insert hyperlink"
+        actions={[
+          <FlatButton
+            label="Cancel"
+            primary
+            onTouchTap={toggleDialog}
+          />,
+          <FlatButton
+            label="Ok"
+            primary
+            keyboardFocused
+            disabled={this.state.url === ''}
+            onTouchTap={toggleDialog}
+          />
+        ]}
+        modal={false}
+        open={isDialogOpen}
+        onRequestClose={toggleDialog}
+      >
+        <TextField
+          id="url"
+          onChange={this.onChange}
+          floatingLabelText="Url"
+          style={{
+            width: '100%'
+          }}
+        />
+      </Dialog>
+    )
+  }
 }
-
-export default LinkDialog
