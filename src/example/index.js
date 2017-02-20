48d8608cde8ad1f7dfd462b9e23fac72bdb8337d
lazarev <diedsmiling@gmail.com>
2017-02-20 23:49:49 +0100

    Added styles to example

--- src/example/index.js
+++ src/example/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import AppBar from 'material-ui/AppBar'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'  // eslint-disable-line
+import 'codemirror/lib/codemirror.css' // import codemirror styles
 import MarkdownEditor from '../MarkdownEditor'
+import '../MarkdownEditor/codemirrorOverride.css' //
 
 injectTapEventPlugin()
 
