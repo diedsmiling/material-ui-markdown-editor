87d80808b80e8fbe0dd5c591471ebc8695b50a99
lazarev <diedsmiling@gmail.com>
2018-10-11 23:03:53 +0200

    Add missing types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,16 @@
 export type Selection = {
-    startOffset: number,
-    endOffset: number
-}
+  startOffset: number,
+  endOffset: number,
+};
+
+export type limitOnNode = {
+  position: number,
+  node: Node,
+};
 
 export type SelectionSpy = {
   startFound: boolean,
-  currentChar: number
-}
+  currentChar: number,
+  start?: limitOnNode,
+  end?: limitOnNode,
+};
