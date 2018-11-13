efc83facf9fb52d0e93bb40ca119f36bebebd78e
diedsmiling <diedsmiling@gmail.com>
2018-11-13 09:44:15 +0100

    Refactor

--- src/helpers/selection.tsx
+++ src/helpers/selection.tsx
@@ -1,21 +1,45 @@
 import { Selection, SelectionSpy } from '../types';
 
+const isNodeByType = (nodeType: number) => (node: Node) => node.nodeType === nodeType;
+
+const isNodeByTag = (tag: string) => (node: Node) => node.nodeName === tag;
+
+const isTextNode = isNodeByType(Node.TEXT_NODE);
+
+const isElementNode = isNodeByType(Node.ELEMENT_NODE);
+
+const isBr = isNodeByTag('BR');
+
+const isStart = (selection: Selection, currentChar: number, nextChar: number) => (
+  selection.startOffset >= currentChar && selection.startOffset <= nextChar
+)
+
+const isEnd = (selection: Selection, currentChar: number, nextChar: number) => (
+  selection.endOffset >= currentChar && selection.endOffset <= nextChar
+)
+
 function traverseAndRestore(
   node: Node,
   selectionSpy: SelectionSpy,
   selection: Selection,
 ): SelectionSpy {
   return [...node.childNodes].reduce((acc: SelectionSpy, node: Node) => {
-    if (node.nodeType === Node.TEXT_NODE) {
+    console.log(node.nodeType);
+
+    let { startFound, start, end } = acc;
+    const { currentChar } = acc;
+
+    if (isElementNode(node) && isBr(node)) {
+       // acc.start.position = acc.start.position + 1;
+       // acc.end.position = acc.end.position + 1;
+    }
+
+    if (isTextNode(node)) {
       let { startFound, start, end } = acc;
       const { currentChar } = acc;
       const nextChar = currentChar + node.textContent.length;
 
-      if (
-        !startFound &&
-        selection.startOffset >= currentChar &&
-        selection.startOffset <= nextChar
-      ) {
+      if (!startFound && isStart(selection, currentChar, nextChar)) {
         startFound = true;
         start = {
           node,
@@ -23,7 +47,7 @@ function traverseAndRestore(
         };
       }
 
-      if (startFound && selection.endOffset >= currentChar && selection.endOffset <= nextChar) {
+      if (startFound && isEnd(selection, currentChar, nextChar)) {
         end = {
           node,
           position: selection.endOffset - currentChar,
@@ -44,7 +68,7 @@ function traverseAndRestore(
 
 export function restoreSelection(node: Node, selection: Selection) {
 
-  const res = traverseAndRestore(
+  const restoredSelection = traverseAndRestore(
     node,
     { startFound: false, currentChar: 0 },
     selection,
@@ -53,8 +77,8 @@ export function restoreSelection(node: Node, selection: Selection) {
   const range = document.createRange();
   range.setStart(node, 0);
   range.collapse(true);
-  range.setEnd(res.end.node, res.end.position);
-  range.setStart(res.start.node, res.start.position);
+  range.setEnd(restoredSelection.end.node, restoredSelection.end.position);
+  range.setStart(restoredSelection.start.node, restoredSelection.start.position);
   const sel = window.getSelection();
   sel.removeAllRanges();
   sel.addRange(range);
